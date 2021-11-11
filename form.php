f49637171b17b738dc4bc3ad235a4c9a25e6fd9f
sadra-hub <84704927+sadra-hub@users.noreply.github.com>
2021-11-11 02:43:32 -0500

    there was a (silly) problem with the code

--- form.php
+++ form.php
@@ -1,33 +1,18 @@
 <?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-?>
-
-
-<?php
-function IsInjected($str)
+if(!isset($_POST['submit']))
 {
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
 }
 
 if(IsInjected($visitor_email))
@@ -35,28 +20,42 @@ if(IsInjected($visitor_email))
     echo "Bad email value!";
     exit;
 }
-?>
+
+$email_from = 'support@energyasa.ir';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+    
+$to = "sadra.moosavilar@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
 
 
-
-<?php
-	$email_from = 'support@energyasa.ir';
-
-	$email_subject = "New Form submission";
-
-	$email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
-?>
-
-
-<?php
-
-  $to = "sadra.moosavilar@gmail.com";
-
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
-
- ?>
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
